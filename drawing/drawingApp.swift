30f843de27184e82b4a413f40c45f1db59667d18
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2022-12-24 19:05:50 +0200

    Initial Commit

---
+++ drawing/drawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawingApp.swift
+//  drawing
+//
+//  Created by Hamza Osama on 24/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct drawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
