b9c3916411411871b41bfd1c0497f9c2c9ed4ec2
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2022-12-26 16:49:42 +0200

    Created a shape ( a circle and a triangle) and adjusted the circle measurements and created a custom color

--- drawing/ContentView.swift
+++ drawing/ContentView.swift
@@ -6,17 +6,55 @@
 //
 
 import SwiftUI
+import Foundation
+
+struct customColor {
+    static let blueSpecial = Color("blueSpecial")
+}
+
+
+
+struct triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct arc: Shape {
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockWise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let rotationAdjustments = Angle.degrees(90)
+        let modifiedStartAngle = startAngle - rotationAdjustments
+        let modifiedEndAngel = endAngle - rotationAdjustments
+        
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStartAngle, endAngle: modifiedEndAngel, clockwise: !clockWise )
+        
+        return path
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
-            path.closeSubpath()
-        }
-        .stroke(.orange,lineWidth: 5)
+        arc(startAngle: .degrees(0), endAngle: .degrees(360), clockWise: true)
+            .stroke(customColor.blueSpecial, lineWidth: 7)
+            .frame(width: 300, height: 300)
+        
+//        triangle()
+//            .stroke(customColor.blueSpecial, style: StrokeStyle(lineWidth: 15, lineCap: .round, lineJoin: .round))
+//            .frame(width: 250, height: 350)
     }
 }
 
