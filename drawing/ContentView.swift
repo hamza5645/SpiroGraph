bc9b9da75025db508de7c33cc82207c3f3039f85
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2023-01-09 15:40:05 +0200

    used Image Paint

--- drawing/ContentView.swift
+++ drawing/ContentView.swift
@@ -7,50 +7,15 @@
 
 import SwiftUI
 
-struct flower: Shape {
-    var petalOffset = -20.0
-    var petalWidth = 100.0
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-            
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-            let rotatedPetal = originalPetal.applying(position)
-            
-            path.addPath(rotatedPetal)
-        }
-        
-        return path
-    }
-}
-
 struct customColor {
     static let blueSpecial = Color("blueSpecial")
 }
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
-    
     var body: some View {
-        VStack {
-            flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(customColor.blueSpecial, style: FillStyle(eoFill: true))
-//                .stroke(customColor.blueSpecial, lineWidth: 2)
-            
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
-            
-        }
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("example"), sourceRect: CGRect(x: 0, y: 0.25, width: 0.4, height: 1),scale: 0.2), lineWidth: 60)
+            .frame(width: 300, height: 300)
     }
 }
 
