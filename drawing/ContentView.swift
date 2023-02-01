39fe49c18670d72f43f156fb261c2989cdfea461
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2023-02-01 20:17:20 +0200

    created a slider that controls the saturation and blueness of the image

--- drawing/ContentView.swift
+++ drawing/ContentView.swift
@@ -13,25 +13,11 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            ZStack {
-                Circle()
-                    .fill(Color(red: 1, green: 0, blue: 0))
-                    .frame(width: 200 * amount)
-                    .offset(x: -50, y: -80)
-                    .blendMode(.screen)
-                
-                Circle()
-                    .fill(Color(red: 0, green: 1, blue: 0))
-                    .frame(width: 200 * amount)
-                    .offset(x: 50, y: -80)
-                    .blendMode(.screen)
-                
-                Circle()
-                    .fill(Color(red: 0, green: 0, blue: 1))
-                    .frame(width: 200 * amount)
-                    .blendMode(.screen)
-            }
-            .frame(width: 300, height: 300)
+            Image("example")
+                .resizable()
+                .frame(width: 200, height: 200)
+                .saturation(amount)
+                .blur(radius: (1 - amount * 20))
             
             Slider(value: $amount)
                 .padding()
