90ef071fe66b1de5627a4dcaaf7fb90529434dc3
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2023-02-01 20:00:27 +0200

    made three colored circles that intersect using an offset slider creating true colors

--- drawing/ContentView.swift
+++ drawing/ContentView.swift
@@ -7,51 +7,36 @@
 
 import SwiftUI
 
-struct colorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
-    
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) {value in
-                Circle()
-                    .inset(by: Double(value))
-                    .strokeBorder(
-                        LinearGradient(gradient: Gradient(colors: [
-                            color(for: value, brightness: 1),
-                            color(for: value, brightness: 0.5),
-                        ]),
-                                       startPoint: .top,
-                                       endPoint: .bottom
-                        ),
-                        lineWidth: 2
-                    )
-            }
-        }
-        .drawingGroup()
-    }
-    
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(steps) + amount
-        
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-        
-        return(Color(hue: targetHue, saturation: 1, brightness: brightness))
-    }
-}
-
 struct ContentView: View {
-    @State private var colorCycle = 0.0
+    @State private var amount = 0.0
+    
     
     var body: some View {
         VStack {
-            colorCyclingCircle(amount: colorCycle)
-                .frame(width: 400, height: 400)
+            ZStack {
+                Circle()
+                    .fill(Color(red: 1, green: 0, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(Color(red: 0, green: 1, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(Color(red: 0, green: 0, blue: 1))
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
             
-            Slider(value: $colorCycle)
+            Slider(value: $amount)
+                .padding()
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
