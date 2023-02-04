7db56cc608cfdaa513901e8d51c61371e5cb944a
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2023-02-04 10:49:53 +0200

    created a simple trapezoid and animated it

--- drawing/ContentView.swift
+++ drawing/ContentView.swift
@@ -7,22 +7,38 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var amount = 0.0
+struct trapezoid: Shape {
+    var insetAmount: Double
     
+    var animatableData: Double {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
+    }
+}
+
+struct ContentView: View {
+    @State private var insetAmount = 50.0
     
     var body: some View {
-        VStack {
-            Image("example")
-                .resizable()
-                .frame(width: 200, height: 200)
-                .saturation(amount)
-                .blur(radius: (1 - amount * 20))
-            
-            Slider(value: $amount)
-                .padding()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                    insetAmount = Double.random(in: 10...90)
+                }
+            }
     }
 }
 
